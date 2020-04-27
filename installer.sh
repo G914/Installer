4591c2fe92f58853e3d9d8244a84241f73e313e5
G914 <64303694+G914@users.noreply.github.com>
2020-04-27 11:18:52 +0530

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,12 @@
+pkg install git -y
+pkg install curl -y
+pkg install php -y
+pkg install python -y
+pkg install python2 -y
+pkg install wget -y
+pkg install nano -y
+pkg install hydra -y
+pkg install w3m -y
+pkg install tor -y
+
+
